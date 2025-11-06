f562c284bd59238681a06640dedd0e3cab55778d
vedha <mvedha98@gmail.com>
2025-11-06 12:00:21 +0530

    update tools menu style

--- renderer.js
+++ renderer.js
@@ -3,6 +3,6 @@ import * as THREE from 'three';
 export function createRenderer() {
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor(0xaaaaaa);
+  renderer.setClearColor("#EDF2F0");
   return renderer;
 }
